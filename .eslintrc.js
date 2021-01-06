ea469db79cc6b2482a7c3b0cf71d660ef93af4ef
tjnk24 <tjnk24@gmail.com>
2021-01-07 02:52:38 +0300

    modifying configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     ],
     "rules": {
         'indent': ['error', 2],
+        'no-unused-vars': 'warn',
         'import/no-unresolved': 'off',
         'import/extensions': 'off',
         'react/prop-types': 'off',
